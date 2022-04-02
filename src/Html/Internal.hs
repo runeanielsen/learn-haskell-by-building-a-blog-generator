08946f2b58360e402c47b392086318f5052fe16b
Rune Nielsen <runenielsen@runbox.com>
2022-04-02 10:57:57 +0200

    finished chapter 3.8

--- src/Html/Internal.hs
+++ src/Html/Internal.hs
@@ -22,6 +22,17 @@ p_ = Structure . el "p" . escape
 h1_ :: String -> Structure
 h1_ = Structure . el "h1" . escape
 
+code_ :: String -> Structure
+code_ = Structure . el "pre"
+
+ul_ :: [Structure] -> Structure
+ul_ =
+  Structure . el "ul" . concat . map (el "li" .getStructureString)
+
+ol_ :: [Structure] -> Structure
+ol_ =
+  Structure . el "ol" . concat . map (el "li" .getStructureString)
+
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
